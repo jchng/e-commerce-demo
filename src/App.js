dcb777ac7b38d1e2e09f157e9bacdf199c7c1456
Jarret Jheng Ch'ng <jarretchng02@gmail.com>
2021-03-12 11:16:20 +1100

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Products from './components/Products'
 
+import { Products, Navbar } from './components';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
+
     </div>
   )
 }
